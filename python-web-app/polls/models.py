7c1227f64c29516dc0d699530b82bea3b4cb8a15
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-09-25 13:34:10 -0500

    more

--- python-web-app/polls/models.py
+++ python-web-app/polls/models.py
@@ -4,10 +4,21 @@ from django.db import models
 from django.utils import timezone
 
 
+# Each model roughly corresponds to a DB table.
+# Each attribute is a DB field.
+  # PITFALL: Attribute names must not conflict with the API
+  # https://docs.djangoproject.com/en/3.0/ref/models/instances/
+  # (e.g. clean, save, delete).
+# Primary key fields are automatically generated, as integers,
+  # but this can be overridden.
+
 class Question(models.Model):
   question_text = models.CharField(max_length=200)
+    # To see all fields built into Django (custom ones are possible too):
+    # https://docs.djangoproject.com/en/3.0/ref/models/fields/#model-field-types
   pub_date = models.DateTimeField('date published')
-    # the only field with an optional human-readable name defined
+    # all fields accept an optional human-readable name
+    # (usually as the first argument)
   def __str__(self):
     return self.question_text
   def was_published_recently(self):
@@ -17,7 +28,8 @@ class Question(models.Model):
              <= t )
 
 class Choice(models.Model):
-  question = models.ForeignKey(Question, on_delete=models.CASCADE)
+  question = ( # refers to another table
+      models.ForeignKey(Question, on_delete=models.CASCADE) )
   choice_text = models.CharField(max_length=200)
   votes = models.IntegerField(default=0)
   def __str__(self):
