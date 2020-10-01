0f4ea911e7f9901ab79cbbaacc6cc52512480004
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-10-01 12:23:25 -0500

    more

--- python-web-app/polls/urls.py
+++ python-web-app/polls/urls.py
@@ -7,28 +7,30 @@ app_name = 'polls'
 
 urlpatterns = [
 
+
   ####
   #### The index
   ####
 
-  path( '', # The (portion of the) path (after the domain name and
-            # whatever else called this path() function).
-            # In this case it corresponds to the post-domain path "/polls/".
+  path( '', # Corresponds to the post-domain path "/polls/".
 
-        # any of the following works
-#       views.index_1,
-#       views.index_2,
-#       views.index_3,
-        views.IndexView.as_view(),
+        ( # pick one of the following
+          # views.index_1,
+          # views.index_2,
+          # views.index_3,
+          views.IndexView.as_view()
+          ),
 
-        name='index'), # Optional. Useful for `URL reversing`:
+        name='index'), # Optional. Useful for `URL reversing`.
+        # See "demonstrateReverse" below, or the reading here:
         # https://docs.djangoproject.com/en/3.1/topics/http/urls/#naming-url-patterns
 
 
   ####
   #### The detail (of a Question)
-  #### These correspond to a post-domain path like "/polls/5/".
   ####
+
+  # These correspond to a post-domain path like "/polls/5/".
 
   # Pick one of these
 #  path('<int:question_id>/', views.detail_1, name='detail'),
@@ -41,22 +43,36 @@ urlpatterns = [
 
 
   ####
-  #### The (evolution of the page displaying)
-  #### results (of submitting a vote)
-  #### These correspond to a post-domain path like "/polls/5/".
+  #### Results (of submitting a vote)
   ####
 
-  # e.g. /polls/5/results/
-  # Pick one of these:
-  # path( '<int:pk>/results/', views.ResultsView.as_view(), name='results' ),
-  path( '<int:question_id>/results/', views.results_1, name='results'),
+  # These correspond to a post-domain path like "/polls/5/".
 
-  # e.g. /polls/5/vote/
+  # Pick one of these:
+  path( '<int:question_id>/results/', views.results_1, name='results'),
+  # path( '<int:pk>/results/', views.ResultsView.as_view(), name='results' ),
+
+
+  ####
+  #### The vote action
+  ####
+
+  # This does not correspond to any view.
+  # Visiting this URL is achieved by submitting a vote from 'detail'.
+  # Once the vote action is complete, the user is taken to 'results'.
+
+  # These patterns correspond to a post-domain URL like /polls/5/vote/
+
   path( '<int:question_id>/vote/',
         views.vote,
         name='vote'),
 
-  # http://127.0.0.1:8000/polls/1/1/demonstrateReverse/1/
+
+  ####
+  #### A page to demonstrate django's 'reverse' function
+  ####
+
+  # Http://127.0.0.1:8000/polls/1/1/demonstrateReverse/1/
   path( '<int:a>/<int:b>/demonstrateReverse/<int:c>/',
         views.demonstrateReverse,
         name='nameOfUrlToDemonstrateReverse'),
